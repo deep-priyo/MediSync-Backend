23d5a40f39d56a3b3d01ce4ba083a29b36875db1
deep_priyo <priyadeepmullick@gmail.com>
2025-02-27 16:27:59 +0530

    fixed port error

--- server.py
+++ server.py
@@ -69,4 +69,5 @@ def get_report():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5001)), debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Use Render's provided PORT
+    app.run(host="0.0.0.0", port=port)
